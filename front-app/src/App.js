f3fa3aaeb4de22ab0ad5cbbf7be71004acb4e460
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-04-23 17:32:24 +0530

    react : initial commit

---
+++ front-app/src/App.js
@@ -0,0 +1,10 @@
+
+function App() { // react component
+  return ( // JSX
+    <div>  
+      <h1> Welcome react </h1>      
+    </div>
+  );
+}
+
+export default App;
--- rest-api/index.js
+++ rest-api/index.js
@@ -1,1 +1,34 @@
-console.log(`hello world`)
+const express = require('express') // importing express
+const mysql   = require('mysql');
+const connection = mysql.createConnection({
+    user     : 'root',
+    password : 'root',
+    database : 'mern_crash_cousre_db'
+});
+
+connection.connect( err => {
+    if(err) {
+        console.log('There is a problem in connecting')
+        console.log(err)
+    }
+
+    else 
+        console.log('Connected Successfuly')
+});
+
+
+
+const app = express() // initilizing express
+
+app.get('/abc', (req, res) => { // accepting request on path /
+    const obj = { msg : 'hello' } // creating result object
+    res.json(obj) // returning a response to client
+    // JSON : JavaScript Object Notation
+}) //http//:localhost:3000/abc
+
+app.listen(3000, function() { // http//:localhost:3000
+    console.log(`Listening on port 3000`)
+})
+
+// REST - REpresentational State Transfer
+// RMM - 
